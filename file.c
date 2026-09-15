374ad9a783dfd5b3a67a03ef08699c5c7f331408
Priyansh <saxenapriyansh95@gmail.com>
2026-09-15 10:54:57 +0530

    created the primary structure

---
+++ file.c
@@ -0,0 +1,28 @@
+// importing libraries
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h> 
+#include<unistd.h>
+
+int main() {
+    
+    // step 1: creae the target file 
+
+        // open file 
+
+        // use fprintf to put some text inside it, then close it 
+
+    // step 2: arm the bomb 
+
+    // step 3: countdown loop 
+    // capture the current time using time_t
+    // calculate the difference between the current time and the file creation time 
+    // print a countdown message to the terminal 
+
+    // step 4: detonation (delete the file)
+    // uses remove()
+    // print success 
+
+
+}
